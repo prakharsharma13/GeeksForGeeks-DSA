726e0b17228382897aff047cb75aa01bc573b419
Prakhar Sharma <115884239+prakharsharma13@users.noreply.github.com>
2024-09-09 00:23:38 +0000

    09-09-2024

---
+++ sort-an-array-of-0s-1s-and-2s/sort-an-array-of-0s-1s-and-2s.java
@@ -0,0 +1,28 @@
+class Solution {
+    // Function to sort an array of 0s, 1s, and 2s
+    public void sort012(ArrayList<Integer> arr) {
+        // code here
+         int count0 = 0;
+        int count1 = 0;
+        int count2 = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr.get(i) == 0) {
+                count0++;
+            } else if (arr.get(i) == 1) {
+                count1++;
+            } else if (arr.get(i) == 2) {
+                count2++;
+            }
+        }
+        arr.clear();
+        for (int i = 0; i < count0; i++) {
+            arr.add(0);
+        }
+        for (int i = 0; i < count1; i++) {
+            arr.add(1);
+        }
+        for (int i = 0; i < count2; i++) {
+            arr.add(2);
+        }
+    }
+}
