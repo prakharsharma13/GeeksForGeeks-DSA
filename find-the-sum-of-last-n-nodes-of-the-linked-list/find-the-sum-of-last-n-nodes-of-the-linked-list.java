9c89887487b8c5f3b3966387b0db6d87ba37284f
Prakhar Sharma <115884239+prakharsharma13@users.noreply.github.com>
2024-10-23 15:21:11 +0000

    23-10-2024

---
+++ find-the-sum-of-last-n-nodes-of-the-linked-list/find-the-sum-of-last-n-nodes-of-the-linked-list.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    // Return the sum of last k nodes
+    public int sumOfLastN_Nodes(Node head, int n) {
+        int totalNodes = 0;
+        Node current = head;
+        
+        while(current != null){
+            current = current.next;
+            totalNodes++;
+        }
+        
+        int target = totalNodes - n;
+        current = head;
+        
+        while(target > 0){
+            current = current.next;
+            target--;
+        }
+        
+        int sum = 0;
+        
+        while(current != null){
+            sum += current.data;
+            current = current.next;
+        }
+        
+        return sum;
+    }
+}
