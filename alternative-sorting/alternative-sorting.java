ecc53e9ac9423aebf01cb197cf70bdda18c6efed
Prakhar Sharma <115884239+prakharsharma13@users.noreply.github.com>
2024-10-25 03:03:39 +0000

    25-10-2024

---
+++ alternative-sorting/alternative-sorting.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static ArrayList<Integer> alternateSort(int[] arr) {
+        ArrayList<Integer> list = new ArrayList<>();
+        int n = arr.length;
+        Arrays.sort(arr);
+        int left=0;
+        int right = n-1;
+        while(left <= right){
+            if(left == right){
+                list.add(arr[left++]);
+                return list;
+            }
+            list.add(arr[right--]);
+             list.add(arr[left++]);
+        }
+        return list;
+    }
+}
+
