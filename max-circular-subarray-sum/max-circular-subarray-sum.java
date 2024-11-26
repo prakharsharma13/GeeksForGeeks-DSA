fd67dae6eabecca40ed629f641ee9618e2a64bd4
Prakhar Sharma <115884239+prakharsharma13@users.noreply.github.com>
2024-11-26 04:41:17 +0000

    26-11-2024

---
+++ max-circular-subarray-sum/max-circular-subarray-sum.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    // a: input array
+    // n: size of array
+    // Function to find maximum circular subarray sum.
+   public int circularSubarraySum(int arr[]) {
+    int sum1 = 0, sum2 = 0;
+    int maxi = Integer.MIN_VALUE, mini = Integer.MAX_VALUE;
+    int total = 0, n = arr.length;
+
+    for (int i = 0; i < n; i++) {
+        total += arr[i];
+        sum1 += arr[i];
+        maxi = Math.max(sum1, maxi);
+        sum1 = Math.max(sum1, 0);
+        sum2 += arr[i];
+        mini = Math.min(sum2, mini);
+        sum2 = Math.min(sum2, 0);
+    }
+    if (total == mini) {
+        return maxi;
+    }
+    return Math.max(maxi, total - mini);
+
+
+    
+}
+}
