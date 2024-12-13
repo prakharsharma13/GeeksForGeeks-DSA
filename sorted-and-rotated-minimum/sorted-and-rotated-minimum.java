8954bcafb2cb1298e19b90806474a9cba47d1fd9
Prakhar Sharma <115884239+prakharsharma13@users.noreply.github.com>
2024-12-13 13:48:55 +0000

    13-12-2024

---
+++ sorted-and-rotated-minimum/sorted-and-rotated-minimum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findMin(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+        if (arr[left] < arr[right]) {
+            return arr[left];
+        }
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (mid > 0 && arr[mid] < arr[mid - 1]) {
+                return arr[mid];
+            }
+
+            if (arr[mid] >= arr[left] && arr[mid] > arr[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return arr[left];
+    }
+}
