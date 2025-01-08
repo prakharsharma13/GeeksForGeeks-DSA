23f0bb1a0749ab1f9156464e861905aca65e3535
Prakhar Sharma <115884239+prakharsharma13@users.noreply.github.com>
2025-01-08 03:33:24 +0000

    8-1-2025

---
+++ count-possible-triangles/count-possible-triangles.java
@@ -0,0 +1,25 @@
+class Solution {
+    // Function to count the number of possible triangles.
+    static int countTriangles(int arr[]) {
+        Arrays.sort(arr);
+        int n=arr.length;
+        int cnt=0;
+        for(int i=2;i<n;i++){
+             int j=0;
+             int k=i-1;
+             while(j < k){
+                   int sum=arr[j]+arr[k];
+                   if(sum > arr[i]){
+                       cnt+=k-j;
+                       k--;
+                   }
+                   else{
+                       j++;
+                   }
+             }
+        }
+        
+        return cnt;
+    }
+}
+
