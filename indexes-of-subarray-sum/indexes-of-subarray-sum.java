073e1c24c7b17ed74c873332af5e7bf385da83aa
Prakhar Sharma <115884239+prakharsharma13@users.noreply.github.com>
2025-01-09 03:27:38 +0000

    9-1-2025

---
+++ indexes-of-subarray-sum/indexes-of-subarray-sum.java
@@ -0,0 +1,24 @@
+class Solution {
+    static ArrayList<Integer> subarraySum(int[] arr, int target) {
+        // code here
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        int j=0; int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+            while(sum>target){
+                sum-=arr[j];
+                j++;
+            }
+            if(sum==target){
+                list.add(j+1);
+                list.add(i+1);
+                return list;
+            }
+        }
+        
+        list.add(-1);
+        return list;
+    }
+  
+}
